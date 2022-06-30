2c4b360ad27a0a361a79f47f487e08bdce79d0d1
vignolo_3@hotmail.com <vignolo_3@hotmail.com>
2022-07-01 00:29:11 +0200

    pictures fixed

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,79 +1,44 @@
-
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
 
 export const HeroCard = ({
-    id,
-    superhero,
-    publisher,
-    alter_ego,
-    first_appearance,
-    characters
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
 }) => {
-   
-    
-    const imagePath = `/assets/${id}.jpg`;
-    console.log(imagePath);
-    return (
-        <div className="col animate__animated animate__fadeIn">
-        <div className="card">
-            
-            <div className="row no-gutters">
-                <div className="col-4">
-                    <img src={imagePath} className="card-img" alt={superhero} />
-                </div>
-                <div className="col-8">
+  //   const imagePath = `/assets/${id}.jpg`;
 
-                    <div className="card-body">
+  return (
+    <div className="col animate__animated animate__fadeIn">
+      <div className="card">
+        <div className="row no-gutters">
+          <div className="col-4">
+            <img
+              src={process.env.PUBLIC_URL + `/assets/${id}.jpg`}
+              className="card-img"
+              alt={superhero}
+            />
+          </div>
+          <div className="col-8">
+            <div className="card-body">
+              <h5 className="card-title">{superhero}</h5>
+              <p className="card-text">{alter_ego}</p>
 
-                        <h5 className="card-title">{superhero}</h5>
-                        <p className="card-text">{alter_ego}</p>
+              {alter_ego !== characters && (
+                <p className="text-muted">{characters}</p>
+              )}
 
-                        {
-                            ( alter_ego !== characters ) 
-                                && <p className="text-muted">{ characters }</p>
-                        }
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
 
-                        <p className="card-text">
-                            <small className="text-muted">{ first_appearance }</small>
-                        </p>
-
-                        
-                        <Link to={`/hero/${id}`}>
-                            Más...
-                        </Link>
-
-                    </div>
-
-                </div>
+              <Link to={`/hero/${id}`}>Más...</Link>
             </div>
-
+          </div>
         </div>
+      </div>
     </div>
-    )
-}
-
-
-{/* <div className = "card ms-3" style = {{maxWidth: 540}}>
-            <div className= "row no-gutters">
-                <div className= "col-md-4">
-                    <img src= {`./assets/heroes/${id}.jpg`} className="card-img" alt = {superhero} />
-                </div>
-                <div className= "col-md-8">
-                    <div className= "card-body"> 
-                        <h5 className = "card-title">{superhero} </h5>
-                        <p className = "card-text"> {alter_ego} </p>
-                            {
-                                (alter_ego !== characters) && <p className = "card-text">{characters}</p>
-                            }
-                        <p className = "card-text"> 
-                            <small className = "text-muted"> {first_appearance}</small>
-                        </p>
-                        <Link className ="link-secondary" to= {`./hero/${id}`}>
-                            Más...
-                        </Link>  
-                        
-                    </div>
-                </div>
-            </div>
-        </div> */}
+  );
+};
